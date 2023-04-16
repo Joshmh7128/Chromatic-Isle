a5f65c2d2854e7f3aea799902a3f43c6bdfbf725
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-04-16 14:57:34 -0400

    more work on spring

--- Assets/Scripts/PlayerAtRandomTime.cs
+++ Assets/Scripts/PlayerAtRandomTime.cs
@@ -7,12 +7,7 @@ public class PlayerAtRandomTime : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gameObject.GetComponent<AudioSource>().loop = true;
+        gameObject.GetComponent<AudioSource>().time = Random.Range(0, gameObject.GetComponent<AudioSource>().time);
     }
 }
