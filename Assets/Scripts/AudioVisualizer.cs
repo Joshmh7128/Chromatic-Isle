dec1d1fd0844950edadd746830df70a2f2ef5c8d
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-04-18 22:06:45 -0400

    Fall mostly done

--- Assets/Scripts/AudioVisualizer.cs
+++ Assets/Scripts/AudioVisualizer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioVisualizer : MonoBehaviour
+public class AudioVisualizer : PuzzleElement
 {
     [SerializeField] AudioSource source;
     float updateStep = 0.1f;
@@ -16,18 +16,36 @@ public class AudioVisualizer : MonoBehaviour
 
     [SerializeField] Renderer vRenderer; // the material we will be effecting
 
+    // puzzle stuff
+    [SerializeField] bool interactable; // do we function on an interaction basis?
+
     private void Awake()
     {
         clipSampleData = new float[sampleDataLength];
         source = GetComponent<AudioSource>();
+        vRenderer = GetComponent<Renderer>();
     }
 
     private void FixedUpdate()
     {
-        // calculate the loudness
-        CalculateLoudness();
-        // make the material move
-        ProcessMaterialChanges();
+        if (!interactable || interactable && elementIsActive)
+        {
+            // calculate the loudness
+            CalculateLoudness();
+            source.volume = Mathf.Lerp(source.volume, 1, 2 * Time.fixedDeltaTime);
+
+            // make the material move
+            ProcessMaterialChanges();
+        }
+        else
+        {
+            source.volume = Mathf.Lerp(source.volume, 0, 2 * Time.fixedDeltaTime);
+                
+
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(transform.localScale.x, 0.1f, transform.localScale.z), lerpSpeed * Time.fixedDeltaTime);
+
+        }
+
     }
 
     // calculate how loud we are
@@ -58,5 +76,10 @@ public class AudioVisualizer : MonoBehaviour
         //vRenderer.material.SetColor("_EmissionColor", Color.Lerp(vRenderer.material.color, vRenderer.material.color * clipLoudness, lerpSpeed * Time.fixedDeltaTime));
         transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(transform.localScale.x, clipLoudness * scaleMult, transform.localScale.z), lerpSpeed * Time.fixedDeltaTime);
 
+    }
+
+    public override void Activate()
+    {
+        elementIsActive = !elementIsActive;
     }
 }
--- Assets/Scripts/PuzzleElement.cs
+++ Assets/Scripts/PuzzleElement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public abstract class PuzzleElement : MonoBehaviour
 {
     public bool canActivate = true; // can this be activated?
+    public bool elementIsActive; // is this element active?
 
     public virtual void Activate() { } // our public activate function
 }
