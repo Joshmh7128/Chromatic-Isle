26b1cfa7abaab14b2291f236c08b9ed9c18cd457
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-04-23 14:03:47 -0400

    winter bass is easier to hear, summer buttons now glow when active

--- Assets/Scripts/InteractableButton.cs
+++ Assets/Scripts/InteractableButton.cs
@@ -6,13 +6,12 @@ public class InteractableButton : Interactable
 {
     [SerializeField] List<Renderer> renderers = new List<Renderer>(); // our renderers for any visual FX
     public AudioSource audioSource;
-    [SerializeField] bool oneTimeUse; // can we only use this once?
+    [SerializeField] bool oneTimeUse, singletonUse; // can we only use this once?
 
     private void Start()
     {
         audioSource = gameObject.GetComponent<AudioSource>();
         RenderCheck();
-
     }
 
     // when we interact, set ourselves to active
@@ -27,7 +26,11 @@ public class InteractableButton : Interactable
         // play our sound
         audioSource.Play();
 
-        RenderCheck();
+        if (!singletonUse)
+            RenderCheck();
+
+        if (singletonUse)
+            StartCoroutine(RenderTrigger());
 
         // activate the puzzle
         foreach (PuzzleElement element in elements)
@@ -38,14 +41,42 @@ public class InteractableButton : Interactable
 
     void RenderCheck()
     {
-        if (renderers.Count != 0)
-            foreach (Renderer renderer in renderers)
-            {
-                if (activeStatus)
-                    renderer.material = Resources.Load<Material>("Materials/ActiveElement");
+        if (!singletonUse)
+        {
+            if (renderers.Count != 0)
+                foreach (Renderer renderer in renderers)
+                {
+                    if (activeStatus)
+                        renderer.material = Resources.Load<Material>("Materials/ActiveElement");
 
-                if (!activeStatus)
-                    renderer.material = Resources.Load<Material>("Materials/InactiveElement");
-            }
+                    if (!activeStatus)
+                        renderer.material = Resources.Load<Material>("Materials/InactiveElement");
+                }
+        }
     }
+
+    IEnumerator RenderTrigger()
+    {
+        // cannot interact
+        usable = false;
+
+        // turn off then turn on the renderers
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material = Resources.Load<Material>("Materials/InactiveElement");
+        }
+
+        // wait
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        // turn on again
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material = Resources.Load<Material>("Materials/ActiveElement");
+        }
+
+        // can interact
+        usable = true;
+    }
+
 }
